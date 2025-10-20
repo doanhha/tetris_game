f49ce67ca18ffba31aa3e2beee03581c8028fd3f
doanhha <dhavan4444@gmail.com>
2025-10-20 11:09:33 +0700

    update 2

--- Script/index.js
+++ Script/index.js
@@ -175,6 +175,14 @@ const BRICK_LAYOUT = [
         ],
     ],
 ];
+
+const KEYS_CODE = {
+    LEFT: 'ArrowLeft',
+    RIGHT: 'ArrowRight',
+    DOWN: 'ArrowDown',
+    UP: 'ArrowUp'
+}
+
 const WHITE_COLOR_ID = 7;
 const canvas = document.getElementById('board');
 const ctx = canvas.getContext('2d');
@@ -204,27 +212,83 @@ class board {
     };
 };
 
-class Brick {
+class brick {
     constructor(id) {
         this.id = id;
         this.layout = BRICK_LAYOUT[id];
-        this.activeIndex = 0;
-        this.colPos = 0;
-        this.rowPos = 5;
+        this.activeIndex = 2;
+        this.colPos = 3;
+        this.rowPos = 3;
     };
 
     draw() {
         for (let row = 0; row < this.layout[this.activeIndex].length; row++) {
-            for( let col = 0; col < this.layout[this.activeIndex][0].length; col++) {
-                if(this.layout[this.activeIndex] [row] [col] !== WHITE_COLOR_ID) {
-                    board.drawCell(col + this.colPos, row + this.rowPos, this.id)
-                }
-            }
-        }
+            for( let col = 0; col < this.layout[this.activeIndex][row].length; col++) {
+                if(this.layout[this.activeIndex][row][col] !== WHITE_COLOR_ID) {
+                    board.drawCell(col + this.colPos, row + this.rowPos, this.id);
+                };
+            };
+        };
+    };
+
+    clear() {
+        for (let row = 0; row < this.layout[this.activeIndex].length; row++) {
+            for( let col = 0; col < this.layout[this.activeIndex][row].length; col++) {
+                if(this.layout[this.activeIndex][row][col] !== WHITE_COLOR_ID) {
+                    board.drawCell(col + this.colPos, row + this.rowPos, WHITE_COLOR_ID);
+                };
+            };
+        };
+    };
+    moveLeft() {
+        this.clear();
+        this.colPos--;
+        this.draw();
+    };
+    moveRight() {
+        this.clear();
+        this.colPos++;
+        this.draw();
+    };
+    moveDown() {
+        this.clear();
+        this.rowPos++;
+        this.draw();
+    };
+    rotage() {
+        this.clear();
+        this.activeIndex = (this.activeIndex + 1) % 4;
+        this.draw();
     };
 };
 
 board = new board(ctx); 
 board.drawBoard();
+brick = new brick(0);
+brick.draw();
 
+document.addEventListener('keydown' , (e) => {
+    console.log(e);
+    switch (e.code) {
+        case KEYS_CODE.LEFT:
+        brick.moveLeft();
+        break;
+        case KEYS_CODE.DOWN:
+        brick.moveDown();
+        break;
+        case KEYS_CODE.RIGHT:
+        brick.moveRight();
+        break;
+        case KEYS_CODE.UP:
+        brick.rotage();
+        break;
+        default:
+            break;
+    }
+})
+
+// brick.rotage();
+// brick.moveLeft();
+// brick.moveRight();
+// brick.moveDown()
 // console.log(this.layout[this.activeIndex]);
